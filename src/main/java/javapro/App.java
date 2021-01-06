bb38920a261f1b4690379b567b825e4d601cbc5b
sufiyan440 <76608895+sufiyan440@users.noreply.github.com>
2021-01-06 17:35:56 +0530

    Update App.java

--- src/main/java/javapro/App.java
+++ src/main/java/javapro/App.java
@@ -1,32 +1,23 @@
 package javapro;
 
-public class App{  
-public static void main(String args[]){  
-//creating a matrix  
-int original[][]={{1,3,4},{2,4,3},{3,4,5}};    
-    
-//creating another matrix to store transpose of a matrix  
-int transpose[][]=new int[3][3];  //3 rows and 3 columns  
-    
-//Code to transpose a matrix  
-for(int i=0;i<3;i++){    
-for(int j=0;j<3;j++){    
-transpose[i][j]=original[j][i];  
-}    
-}    
-  
-System.out.println("Printing Matrix without transpose:");  
-for(int i=0;i<3;i++){    
-for(int j=0;j<3;j++){    
-System.out.print(original[i][j]+" ");    
-}    
-System.out.println();//new line    
-}    
-System.out.println("Printing Matrix After Transpose:");  
-for(int i=0;i<3;i++){    
-for(int j=0;j<3;j++){    
-System.out.print(transpose[i][j]+" ");    
-}    
-System.out.println();//new line    
-}    
-}}
+public class App{   
+    public static void main(String[] args) {    
+            
+        String str1="Great Power";    
+        StringBuffer newStr=new StringBuffer(str1);    
+            
+        for(int i = 0; i < str1.length(); i++) {    
+                
+            //Checks for lower case character    
+            if(Character.isLowerCase(str1.charAt(i))) {    
+                //Convert it into upper case using toUpperCase() function    
+                newStr.setCharAt(i, Character.toUpperCase(str1.charAt(i)));    
+            }    
+            //Checks for upper case character    
+            else if(Character.isUpperCase(str1.charAt(i))) {    
+                //Convert it into upper case using toLowerCase() function    
+                newStr.setCharAt(i, Character.toLowerCase(str1.charAt(i)));    
+            }    
+        }    
+        System.out.println("String after case conversion : " + newStr);    
+    }    
